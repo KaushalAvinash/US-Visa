bacbb19f12497e22201202d0f99572f8e0ce305e
Kaushal Avinash <avinashkaushal763@gmail.com>
2025-01-26 12:23:26 +0530

    Data Ingestion completed

---
+++ pusher.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import json
+
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+
+import certifi
+ca=certifi.where()
+import pandas as pd
+import numpy as np
+import pymongo
+from us_visa.exception import USvisaException
+from us_visa.logger import logging
+
+class VisaDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise USvisaException(e,sys)
+        
+    def csv_to_json_convertor(self,file_path):
+        try:
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise USvisaException(e,sys)
+        
+    def insert_data_mongodb(self,records,database,collection):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return(len(self.records))
+        except Exception as e:
+            raise USvisaException(e,sys)
+        
+if __name__=="__main__":
+    FILE_PATH="notebook\Visadataset.csv"
+    DATABASE="US_VISA"
+    Collection="visa_data"
+    visaobj=VisaDataExtract()
+    records=visaobj.csv_to_json_convertor(file_path=FILE_PATH)
+    print(records)
+    no_of_records=visaobj.insert_data_mongodb(records,DATABASE,Collection)
+    print(no_of_records)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="Avinash",
+    author_email="avinashkaushal763@gmail.com",
+    packages=find_packages()
+)
