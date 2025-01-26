4b9cd96c4058a02b9cd5d71ca4c3ebd9a09e5f7e
Kaushal Avinash <avinashkaushal763@gmail.com>
2025-01-26 13:30:45 +0530

    data validation through Evidently completed

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -7,7 +7,6 @@ COLLECTION_NAME = "visa_data"
 PIPELINE_NAME: str = "usvisa"
 ARTIFACT_DIR: str = "artifact"
 
-MODEL_FILE_NAME = "model.pkl"
 
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
@@ -31,4 +30,11 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
